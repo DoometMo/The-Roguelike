5a1b0b9f8ce2836996f6196574d30961644a419c
DoometMo <defiantlylive@gmail.com>
2022-10-15 19:55:24 -0500

    FOV added

--- roguelike/src/player.rs
+++ roguelike/src/player.rs
@@ -1,25 +1,28 @@
 use rltk::{VirtualKeyCode, Rltk};
 use specs::prelude::*;
-use super::{Position, Player, TileType, xy_idx, State};
-use std::cmp::{min, max};
+use std::cmp::{max, min};
+use super::{Position, Player, Viewshed, TileType, State, Map};
 
 // Player movement
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let map = ecs.fetch::<Map>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y); //  if it is NOT a Wall: Player can move there
-        if map[destination_idx] != TileType::Wall { // if it is Wall: Player can NOT move there
+    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79 , max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
+
+            viewshed.dirty = true;
         }
     }
 }
 
 pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // Player movement
+    // Player Input
     match ctx.key {
         None => {} // Nothing happened
         Some(key) => match key {
